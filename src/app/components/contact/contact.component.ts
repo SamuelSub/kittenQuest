76c88abb77a678b07db1c8b8dd41310520b6d274
samuel <samsubas01@gmail.com>
2022-04-21 13:29:13 +0300

    fixed alert boxes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -13,13 +13,42 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showSuccessBox = false;
+  successMsg = 'Email Sent Successfully!'
+  
+  showAlertBox = false;
+  validationMsg = ''
+
+  displayAlertBox() {
+    this.showAlertBox = true;
+    setTimeout(() => {
+      this.showAlertBox = false;
+    }, 2500);
+  }
+
+  displaySuccessBox() {
+    this.showSuccessBox = true;
+    setTimeout(() => {
+      this.showSuccessBox = false
+    }, 2500);
+  }
+
   handleContactForm(formValues: any) {
     // These keys should not be publicly available like i am doing, it's just for a project.
+    if(formValues.terms === '') {
+      this.validationMsg = 'You Need To Agree With The Terms & Conditions!';
+      this.displayAlertBox()
+      return
+    }  
+
     emailjs.send('service_ro80pyo', 'template_7ywl66f', formValues, 'user_4AtxMgTsmU9RsnzhIN1z1')
       .then((result: EmailJSResponseStatus) => {
         console.log(result.text);
+        this.displaySuccessBox()
       }, (error) => {
         console.log(error.text);
+        this.validationMsg = 'Email Wasnt Sent Please Try Again';
+        this.displayAlertBox();
       });
   }
 
